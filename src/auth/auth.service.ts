9e3666432fd6d9ac1ad59c8d476f0c31fd406122
Aldo Dominguez <aldodominguezchz@gmail.com>
2022-05-06 17:17:40 -0500

    add signup and signin endpoints

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDTO } from './DTOs/auth-credentials.dto';
+import { UsersRepository } from './users.repository';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(UsersRepository) private userRepo: UsersRepository,
+  ) {}
+
+  public async signUp(authCredentialsDTO: AuthCredentialsDTO): Promise<void> {
+    return this.userRepo.createUser(authCredentialsDTO);
+  }
+
+  public async signIn(authCredentialsDTO: AuthCredentialsDTO): Promise<string> {
+    const { username, password } = authCredentialsDTO;
+    const user = await this.userRepo.findOne({ username });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return 'sucess';
+    } else {
+      throw new UnauthorizedException('Invalid username and/or password');
+    }
+  }
+}
---
+++ src/auth/users.repository.ts
@@ -0,0 +1,35 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDTO } from './DTOs/auth-credentials.dto';
+import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
+
+@EntityRepository(User)
+export class UsersRepository extends Repository<User> {
+  public async createUser(
+    authCredentialsDTO: AuthCredentialsDTO,
+  ): Promise<void> {
+    const { username, password } = authCredentialsDTO;
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({
+      username,
+      password: hashedPassword,
+    });
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('The username is already taken');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+}
